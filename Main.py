1bfbce4a3a2766d3a803ef8ffcd0aefce2903f15
Nikita Rana <32420201+NikitaRana@users.noreply.github.com>
2018-05-28 00:13:39 +0530

    Added new functions

--- Main.py
+++ Main.py
@@ -1,7 +1,5 @@
-#Endpoint == URL
-
 #------------------------------------------------------------------------------------------------------------------------------------
-#                               Packages installed
+#                    List of packages imported
 #------------------------------------------------------------------------------------------------------------------------------------
 
 from twitter import Twitter,OAuth
@@ -9,88 +7,104 @@ import pprint
 import tweepy
 import textblob
 from textblob import TextBlob
+import json
 
 #------------------------------------------------------------------------------------------------------------------------------------
-#                               Function to count the numbers of followers
+#                       Inclusions of files
 #------------------------------------------------------------------------------------------------------------------------------------
 
-get_num_followers(query):
-    import json
-    num_followers = 0
-    tweets = get_tweets(query)
-    for each_tweet in tweets[]:
-        print(each_tweet['user']['followers_count'])
-        num_followers = += each_tweet['user']['followers_count']
-    return num_followers
+global f
+global f2
+f = open('positive.txt','w')
+f2 = open('negative.txt','w')
 
 #------------------------------------------------------------------------------------------------------------------------------------
-
+#                    Function to search for tweets
 #------------------------------------------------------------------------------------------------------------------------------------
-
-def get_tweets(query):
+tweets = []
+def GetSearch(query):
     tweets = twitter.search.tweets(q='#' + query)
     return tweets
 
 #------------------------------------------------------------------------------------------------------------------------------------
-
+#                    Function to count the numbers of followers
 #------------------------------------------------------------------------------------------------------------------------------------
 
-oauth = tweepy.OAuthHandler(API_KEY,API_SECRET)
-b = tweepy.OAuthHandler('134','213')
-oauth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
-api = tweepy.API(oauth)
-public_tweets = api.search("#DeleteFacebook")
-new_tweets = api.user_timeline(screen_name = '@DonalTrump',max_id=oldest)
-for tweet in new_tweets:
-    print(tweet.text)
-    analyse = TextBlob(tweets.text)
-    print(analyse,sentiment)
-    print("-----------------")
+def Get_number_followers(query):
+    num_followers = 0
+    tweets = GetSearch(query)
+    for each_tweet in tweets[]:
+        print(each_tweet['user']['followers_count'])
+        num_followers += each_tweet['user']['followers_count']
+    return num_followers
 
 #------------------------------------------------------------------------------------------------------------------------------------
-#                   Determine the Sentiment of People tweeting using a certain hashtag
+#                   Function to determine the location,language and time zone
 #------------------------------------------------------------------------------------------------------------------------------------
-
-for tweet in public_tweets:
-    print(tweet.text)
-    print(tweet.created_at)
-    print(tweet.author)
-    analyse = TextBlob(tweets.text)
-    print(analyse,sentiment)
-    print("-----------------")
+def Demographic_data ( user_input ):
+    tweets = GetSearch(user_input)
+    for each_tweet in tweets:
+        print(each_tweet['user']['location'])
+        print(each_tweet['user']['timezone'])
+        print(each_tweet['user']['language'])
 
 #------------------------------------------------------------------------------------------------------------------------------------
+#                   Function to determine the location,language and time zone
+#------------------------------------------------------------------------------------------------------------------------------------
+def  ModiVSTrump():
+    oauth = tweepy.OAuthHandler(API_KEY,API_SECRET)
+    b = tweepy.OAuthHandler('134','213')
+    times = 0
+    oauth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+    api = tweepy.API(oauth)
+    public_tweets = api.search("#US")
+    new_tweets = api.user_timeline(screen_name = '@Modi',count=200)
+    for tweet in new_tweets:
+        print(tweet.text)
+        times+=times
+    return  times
 
+#------------------------------------------------------------------------------------------------------------------------------------
+#                    Function to determine the sentimment
+#------------------------------------------------------------------------------------------------------------------------------------
+def sentiment_analysis(query):
 
-alltweets = []
-alltweets.extend(new_tweets)
+    oauth = tweepy.OAuthHandler(API_KEY,API_SECRET)
+    b = tweepy.OAuthHandler('134','213')
+    oauth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+    api = tweepy.API(oauth)
+    public_tweets = api.search("query")
+    new_tweets = api.user_timeline(screen_name = '@DonalTrump',max_id=oldest)
+    for tweet in new_tweets:
+        print(tweet.text)
+        analyse = TextBlob(tweets.text)
+        print(analyse,sentiment)
+        print("-----------------")
+        if analysis.sentiment.polarity > 0:
+            f.write ( "This is positive:> \n" + tweet )
+        elif analysis.sentiment.polarity == 0:
+            f2.write ( "This is Negative:> \n" + tweet )
+        else:
+            print("Well if its less then its a problem")
 
+#------------------------------------------------------------------------------------------------------------------------------------
+#                   Function to determine top usage of words
+#------------------------------------------------------------------------------------------------------------------------------------
+def ModiTopWords():
+    oauth = tweepy.OAuthHandler ( API_KEY, API_SECRET )
+    b = tweepy.OAuthHandler ( '134', '213' )
+    oauth.set_access_token ( ACCESS_TOKEN, ACCESS_TOKEN_SECRET )
+    api = tweepy.API ( oauth )
+    public_tweets = api.search ( "#DeleteFacebook" )
+    new_tweets = api.user_timeline ()
+    for tweet in new_tweets:
+        print(tweet.text)
 
+#------------------------------------------------------------------------------------------------------------------------------------
+#                                          Function to tweet a message
+#------------------------------------------------------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+##def add_tweet(query):
 
 #------------------------------------------------------------------------------------------------------------------------------------
 #                                          Main function program
@@ -99,21 +113,21 @@ def main():
     while(True):
         user_choice = raw_input("1. Count the followers of people tweeting using a certain hashtag.\n"
                                 "2. Determine the location, Timezone and language of people tweeting using a certain hashtag.\n"
-                                "3. Number of times Modi has referred to US in the past 200 Tweets compared to how man...\n"
+                                "3. Number of times Modi has referred to US in the past 200 Tweets\n"
                                 "4. Determine the Sentiment of People tweeting using a certain hashtag.\n"
                                 "5. Top used words by PM Modi on Twitter.\n"
                                 "6. Tweet a message from your account.\n"
                                 "7. Exit\n")
         if user_choice == 1:
             user_input = raw_input("Enter the Hashtag: ")
-            print("\n\n Max. number of people who might have seen this hashtag: %s" %(get_num_followers(user_input)))
+            print("\n\n Max. number of people who might have seen this hashtag: %s" %(Get_number_followers(user_input)))
 
         elif user_choice == 2:
             user_input = raw_input("Enter the Hashtag: ")
-            demographic_data(user_input)
+            Demographic_data(user_input)
 
         elif user_choice == 3:
-            ModiVSTrump()
+            print("\n\nNumber of times Modi mentioned US in the past 200 tweets are %s" %ModiVSTrump())
 
         elif user_choice ==4:
             user_input = raw_input ( "Enter the Hashtag: " )
@@ -132,14 +146,20 @@ def main():
         else:
             print("I didn't get that. Please try again. ")
 
-main()
+def main():
+    if __name__ == '__main__':
+        main()
 # Credentials of the developer for calling the Twitter API
 
-# NOT THE CORRECT ACTUAL KEYS
-API_KEY = '	Djhgbjjhgjjgjhhgjne9yryP'
-API_SECRET = 'agoifghfhfuyfjhgjyhtuygvjhgyu5EERAnbj'
-ACCESS_TOKEN = '84542986786785875-vjfkthfvjhftyR54ksm86K8'
-ACCESS_TOKEN_SECRET = 'zaZqWnckwndfkjsncksdncBifY37wMGFoM9kX'
+# THESE ARE INCORRECT KEYS-------------V
+
+API_KEY = '	Wfjwisfjewkjfwoine9yryP'
+API_SECRET = 'nweknfdklasndkwefeXVmoQfmklewnfkwHKJHEKDWnbj'
+ACCESS_TOKEN = '345578423374982-r63kAG7GMUyRGM0cIDPfSR54ksm86K8'
+ACCESS_TOKEN_SECRET = '4289375293873nckdsnnkjshcskfBifY37wMGFoM9kX'
+
+# THESE ARE INCORRECT KEYS--------------^
+
 
 #The permissions returned by the OAuth class of twitter package in the form of object
 twitter_oouth = OAuth(ACCESS_TOKEN,ACCESS_TOKEN_SECRET,API_KEY,API_SECRET)
@@ -154,8 +174,4 @@ results = twitter.search.tweet(q='digital forensics', geography='28.6315 77.2167
 pp = pprint.PrettyPrinter(indent=2)
 pp.pprint(results)
 
-#1. Search for a hashtag and then determine the total count of the followers of the people who tweeted using that hashtag
-
-
-#2. Determine the demographic(gender,timezone,locale) of people who have tweet using certain hash tag
 
